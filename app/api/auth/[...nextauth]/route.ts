a5200b9e54090cc3dde96ef9ed0b945828c14b30
SergioD <sergio_david2109@hotmail.com>
2024-08-07 18:58:21 -0600

    chore: view login errors in logs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,35 +5,40 @@ import User from '../../../models/user.models'
 import bcrypt from 'bcryptjs'
 
 const authOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text", placeholder: "email" },
-                password: { label: "Password", type: "password", placeholder: "password" }
-            },
-            async authorize(credentials, req) {
-                const email = credentials?.email;
-                const password = credentials?.password as string;
-                await connectDB()
-                const userFound = await User.findOne({ email })
-                if (!userFound) throw new Error('Email not found')
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "email" },
+        password: { label: "Password", type: "password", placeholder: "password" }
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password as string;
+        try {
+          await connectDB()
+          const userFound = await User.findOne({ email })
+          if (!userFound) throw new Error('Email not found')
 
-                const isMatch = await bcrypt.compare(password, userFound.password)
+          const isMatch = await bcrypt.compare(password, userFound.password)
 
-                if (!isMatch) throw new Error('Wrong credentials')
+          if (!isMatch) throw new Error('Wrong credentials')
 
-                return {
-                    id: userFound.id,
-                    name: userFound.username,
-                    email: userFound.email
-                }
-            }
-        })
-    ],
-    pages: {
-        signIn: "/auth/login"
-    }
+          return {
+            id: userFound.id,
+            name: userFound.username,
+            email: userFound.email
+          }
+        } catch (error) {
+          console.log(error)
+          throw new Error('Server Error')
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: "/auth/login"
+  }
 }
 const handler = NextAuth(authOptions)
 
