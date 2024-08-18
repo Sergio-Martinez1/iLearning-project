81b1cd1f8b60c7258d1ac02ed74174d5e067fbdc
SergioD <sergio_david2109@hotmail.com>
2024-08-18 08:50:18 -0600

    feat: add more method for collection api

---
+++ app/api/collections/[id]/route.ts
@@ -0,0 +1,88 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Storage } from "@google-cloud/storage";
+import { connectDB } from '../../../../db/db_config'
+import { getServerSession } from 'next-auth/next'
+import User from '../../../../db/models/user.models'
+import Group from '../../../../db/models/group.models'
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        await connectDB()
+        const group = await Group.findOne({ _id: params.id })
+        return NextResponse.json(group)
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({ message: 'File upload failed' }, { status: 500 });
+    }
+}
+
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    const BUCKET_NAME = process.env.BUCKET_NAME
+    const BUCKET_URL = process.env.BUCKET_URL
+    const session = await getServerSession()
+
+    if (!BUCKET_NAME || !BUCKET_URL) return NextResponse.json({ message: 'Server error' }, { status: 500 })
+    if (!session) return NextResponse.json({ message: 'Not authenticated' }, { status: 401 })
+
+    try {
+        const data = await req.formData()
+        const thumbnail = data.get('thumbnail') as File
+        const thumbnail_url = data.get('thumbnail_url')
+        const name = data.get('name')
+        const description = data.get('description')
+        const topic = data.get('topic')
+        let url = thumbnail_url
+
+        await connectDB()
+        const userSession = await User.findOne({ username: session.user?.name })
+        const currentGroup = await Group.findOne({ _id: params.id })
+
+        if (!currentGroup) return NextResponse.json({ message: 'Collection not found' }, { status: 400 })
+        if (userSession.role !== 'admin') {
+            if (userSession._id !== currentGroup.user) return NextResponse.json({ message: 'Not allowed' }, { status: 403 })
+        }
+
+        if (thumbnail) {
+            const unique_id = crypto.randomUUID()
+            const file_name = `image_${unique_id}`
+            const fileBuffer = Buffer.from(await thumbnail.arrayBuffer())
+            const storage = new Storage();
+            await storage.bucket(BUCKET_NAME).file(file_name).save(fileBuffer, { contentType: thumbnail.type, resumable: false });
+            await storage.bucket(BUCKET_NAME).file(file_name).makePublic();
+            url = `${BUCKET_URL}/${file_name}`
+        }
+
+        currentGroup.name = name;
+        currentGroup.description = description;
+        currentGroup.topic = topic;
+        currentGroup.thumbnail_url = url;
+        await currentGroup.save()
+        return NextResponse.json(currentGroup);
+
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({ message: 'Server error' }, { status: 500 });
+    }
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+    const session = await getServerSession()
+    if (!session) return NextResponse.json({ message: 'Not authenticated' }, { status: 401 })
+
+    try {
+        await connectDB()
+        const userSession = await User.findOne({ username: session.user?.name })
+        const currentGroup = await Group.findOne({ _id: params.id })
+
+        if (!currentGroup) return NextResponse.json({ message: 'Collection not found' }, { status: 400 })
+        if (userSession.role !== 'admin') {
+            if (userSession._id !== currentGroup.user) return NextResponse.json({ message: 'Not allowed' }, { status: 403 })
+        }
+
+        await currentGroup.deleteOne()
+        return NextResponse.json({ message: 'Collection deleted successfully' });
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({ message: 'Server error' }, { status: 500 });
+    }
+}
