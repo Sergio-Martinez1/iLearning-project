b1af9c7051add07d026d03f21a08d4d47a04ea73
SergioD <sergio_david2109@hotmail.com>
2024-08-18 08:42:54 -0600

    feat: add user schema in mongoose

---
+++ db/models/user.models.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+    }
+})
+
+export default mongoose.models.User || mongoose.model('User', userSchema)
