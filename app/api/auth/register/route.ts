cc786b78899b79219d52a730324750ba12c9219e
SergioD <sergio_david2109@hotmail.com>
2024-08-18 08:50:48 -0600

    refactor: add role for users in api route

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, type NextRequest } from 'next/server'
-import { connectDB } from '../../../db/db_config'
-import User from '../../../models/user.models'
+import { connectDB } from '../../../../db/db_config'
+import { getServerSession } from 'next-auth/next'
+import User from '../../../../db/models/user.models'
 import bcrypt from 'bcryptjs'
 
 export async function POST(request: NextRequest) {
@@ -14,10 +15,11 @@ export async function POST(request: NextRequest) {
             return NextResponse.json({ message: "Email already in use" }, { status: 400 })
         }
         const passwordHash = await bcrypt.hash(data.password, 10)
-        const newUser = new User({ username: data.username, email: data.email, password: passwordHash })
+        const newUser = new User({ username: data.username, email: data.email, password: passwordHash, role: "user" })
         await newUser.save()
         return Response.json({ message: 'User created succesfully' })
     } catch (error) {
+        console.log(error)
         return new Response(JSON.stringify({ message: 'Error creating user' }), { status: 500 });
     }
 
