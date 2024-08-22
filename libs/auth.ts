e99fcef29e1da69f8fe454016def2b870454deba
SergioD <sergio_david2109@hotmail.com>
2024-08-22 00:12:37 -0600

    feat: add required field in session

--- libs/auth.ts
+++ libs/auth.ts
@@ -4,6 +4,24 @@ import { connectDB } from '@/db/db_config'
 import User from '@/db/models/user.models'
 import bcrypt from 'bcryptjs'
 
+declare module 'next-auth' {
+    interface Session {
+        role?: string;
+        reactions?: Array<Number>;
+        status?: Boolean;
+        theme?: string;
+    }
+
+    interface Token {
+        role?: string;
+        reactions?: Array<Number>;
+        status?: Boolean;
+        theme?: string;
+    }
+}
+
+
+
 export const authOptions: NextAuthOptions = {
     providers: [
         Credentials({
@@ -25,19 +43,22 @@ export const authOptions: NextAuthOptions = {
 
                     if (!isMatch) throw new Error('Wrong credentials')
 
+                    if (userFound.status == true) throw new Error('User is blocked')
+
                     return {
                         id: userFound._id,
                         name: userFound.username,
                         email: userFound.email,
                     }
                 } catch (error) {
-                    throw new Error('Server Error')
+                    throw new Error((error as Error).message);
                 }
             }
         })
     ],
     pages: {
-        signIn: "/auth/login"
+        signIn: "/login",
+        error: "/register"
     },
     callbacks: {
         async jwt({ token }) {
@@ -47,14 +68,26 @@ export const authOptions: NextAuthOptions = {
                 const userFound = await User.findOne({ username: token.name })
                 if (!userFound) throw new Error('User not found')
                 token.role = userFound.role
+                token.reactions = userFound.reactions
+                token.status = userFound.status
+                token.theme = userFound.theme
                 return token;
             } catch (error) {
-                throw new Error('Server Error')
+                throw new Error((error as Error).message);
             }
         },
         async session({ session, token }) {
             if (token.role) {
                 session.role = token.role as string
+            }
+            if (token.reactions) {
+                session.reactions = token.reactions as Array<Number>
+            }
+            if (token.status != null) {
+                session.status = token.status as Boolean
+            }
+            if (token.theme) {
+                session.theme = token.theme as string
             }
             return session;
         },
