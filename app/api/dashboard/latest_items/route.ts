3e205929a6ce56a6447ea63069afc403f3b3aad1
SergioD <sergio_david2109@hotmail.com>
2024-08-22 11:36:19 -0600

    feat: new route for get latest items

---
+++ app/api/dashboard/latest_items/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from '@/db/db_config'
+import Item from '@/db/models/item.model'
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        await connectDB()
+        const items = await Item.find().populate('user', 'username').populate('group', 'name').sort({ createdAt: -1 }).limit(20)
+        if (!items) return NextResponse.json({ error: 'Items not found' }, { status: 404 });
+        return NextResponse.json(items)
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({ error: 'Server error' }, { status: 500 });
+    }
+}
