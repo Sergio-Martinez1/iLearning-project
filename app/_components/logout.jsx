5c0a7146e54ea5f846b9fdbf7a2d0cb6a4474a4d
SergioD <sergio_david2109@hotmail.com>
2024-08-22 11:28:23 -0600

    refactor: update logout button

--- app/_components/logout.jsx
+++ app/_components/logout.jsx
@@ -1,17 +1,19 @@
 "use client";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { MdLogout } from "react-icons/md";
 
 function LogOutButton() {
   const router = useRouter();
   return (
     <button
+    className="px-1 pl-1.5 py-1 flex items-center justify-center w-fit h-fit"
       onClick={() => {
         signOut();
         router.push('/')
       }}
     >
-      LogOut
+      <MdLogout size={20}></MdLogout>
     </button>
   );
 }
