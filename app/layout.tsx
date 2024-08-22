4aae7e0d8d7944302ee18f2608527bfea3571fde
SergioD <sergio_david2109@hotmail.com>
2024-08-22 00:15:10 -0600

    refactor: separate layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/auth";
-import LogOutButton from '@/app/_components/logout'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,25 +19,9 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions)
 
   return (
-    <html lang="en">
+    <html lang="en" data-theme={session?.theme || "light"}>
       <body className={inter.className}>
         <div className="w-full h-screen flex flex-col">
-          <nav className="w-full bg-[var(--element-color)] h-16 p-2">
-            <ul className="w-full h-full flex justify-end items-center gap-x-4">
-              <li><Link href="/dashboard">Home</Link></li>
-              {session ?
-                <div className="flex gap-x-4 items-center">
-                  <li><Link href={`/profile/${session.user?.name}`}>Profile</Link></li>
-                  <LogOutButton></LogOutButton>
-                </div>
-                :
-                <div className="flex gap-x-4">
-                  <li><Link href="/login">Login</Link></li>
-                  <li><Link href="/register">Sign up</Link></li>
-                </div>
-              }
-            </ul>
-          </nav>
           {children}
         </div>
       </body>
