0345fce9f93ddc7b002618b9a229a2fad6d05b68
SergioD <sergio_david2109@hotmail.com>
2024-08-22 11:38:54 -0600

    feat: new routes for reactions

---
+++ app/api/reactions/item/[id]/route.ts
@@ -0,0 +1,55 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from '@/db/db_config'
+import { getServerSession } from 'next-auth/next'
+import User from '@/db/models/user.models'
+import Item from '@/db/models/item.model'
+
+export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const session = await getServerSession()
+        if (!session) return NextResponse.json({ error: 'Not authenticated' }, { status: 401 })
+
+        await connectDB()
+        const userSession = await User.findOne({ username: session.user?.name })
+        const currentItem = await Item.findOne({ _id: params.id })
+        if (!currentItem) return NextResponse.json({ error: 'Item not found' }, { status: 404 })
+        if (userSession.role !== 'admin') {
+            if (userSession._id !== currentItem.user) return NextResponse.json({ error: 'Not allowed' }, { status: 403 })
+        }
+
+        userSession.reactions.push(params.id)
+        currentItem.reactions += 1
+        await currentItem.save()
+        await userSession.save()
+
+        return NextResponse.json(currentItem)
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ error: 'Server error' }, { status: 500 })
+    }
+}
+
+export async function DELETE(req: NextRequest, { params }: { params: { id: Number } }) {
+    try {
+        const session = await getServerSession()
+        if (!session) return NextResponse.json({ error: 'Not authenticated' }, { status: 401 })
+
+        await connectDB()
+        const userSession = await User.findOne({ username: session.user?.name })
+        const currentItem = await Item.findOne({ _id: params.id })
+        if (!currentItem) return NextResponse.json({ error: 'Item not found' }, { status: 404 })
+        if (userSession.role !== 'admin') {
+            if (userSession._id !== currentItem.user) return NextResponse.json({ error: 'Not allowed' }, { status: 403 })
+        }
+
+        userSession.reactions = userSession.reactions.filter((reactionId: Number) => reactionId !== params.id)
+        currentItem.reactions -= 1
+        await currentItem.save()
+        await userSession.save()
+
+        return NextResponse.json(currentItem)
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ error: 'Server error' }, { status: 500 })
+    }
+}
