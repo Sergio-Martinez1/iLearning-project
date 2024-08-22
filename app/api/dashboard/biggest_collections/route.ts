8354f14750e09ad65cdf127cbb870dc6c6887b6a
SergioD <sergio_david2109@hotmail.com>
2024-08-22 11:35:48 -0600

    feat: new route for get biggest collections

---
+++ app/api/dashboard/biggest_collections/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectDB } from '@/db/db_config'
+import Group from '@/db/models/group.models'
+import Item from '@/db/models/item.model'
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        await connectDB()
+        const result = await Item.aggregate([
+            {
+                $group: {
+                    _id: "$group",
+                    itemCount: {
+                        $count: {}
+                    }
+                }
+            },
+            {
+                $sort: { itemCount: -1 }
+            },
+            { $limit: 5 },
+            {
+                $lookup: {
+                    from: "groups",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "groupDetails"
+                }
+            },
+            { $unwind: "$groupDetails" },
+            {
+                $project: {
+                    _id: 0,
+                    group: "$groupDetails",
+                    itemCount: 1
+                }
+            }
+        ])
+        if (!result) return NextResponse.json({ error: 'Groups not found' }, { status: 404 });
+        return NextResponse.json(result)
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({ error: 'Server error' }, { status: 500 });
+    }
+}
