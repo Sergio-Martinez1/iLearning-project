c7d207cf9952198b144d11cd0ffa2e056aa297d5
SergioD <sergio_david2109@hotmail.com>
2024-08-23 02:22:40 -0600

    fix: styles in home button

--- app/[lng]/(auth)/layout.tsx
+++ app/[lng]/(auth)/layout.tsx
@@ -10,8 +10,8 @@ export default async function RootLayout({
 
   return (
     <div className="w-full h-screen flex flex-col">
-      <nav className="w-full h-fit pt-2 md:pt-6 pb-2 flex justify-center bg-[var(--element-color)] md:bg-transparent">
-        <ul className="w-fit bg-transparent md:bg-[var(--element-color)] py-2 px-4  rounded-2xl h-full md:flex justify-end items-center gap-x-4 grid grid-cols-4 gap-y-2">
+      <nav className="w-full h-fit pt-6 pb-2 flex justify-center bg-transparent">
+        <ul className="w-fit bg-[var(--element-color)] py-2 px-4  rounded-2xl h-full flex justify-end items-center gap-x-4 gap-y-2">
           <li className="hover:text-[var(--accent-color)] col-start-1 col-span-1"><Link href="/dashboard"><MdHome size={25}></MdHome></Link></li>
         </ul>
       </nav>
