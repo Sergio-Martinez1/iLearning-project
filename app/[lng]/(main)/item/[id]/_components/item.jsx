b307edaa4882a8151c3d75fcbc16959dbe974b6c
SergioD <sergio_david2109@hotmail.com>
2024-08-23 02:55:24 -0600

    styles: improve ui

--- app/[lng]/(main)/item/[id]/_components/item.jsx
+++ app/[lng]/(main)/item/[id]/_components/item.jsx
@@ -139,96 +139,102 @@ function ItemDetails({ id, session }) {
   }
 
   return (
-    <section className="max-w-[600px] bg-[var(--element-color)] rounded-2xl p-4 flex flex-col h-full">
-      {item && (
-        <div className="grow">
-          <div className="flex gap-x-4 items-center mb-2">
-            <span className="font-bold text-2xl">{item["name"]}</span>
-            <button
-              onClick={() => {
-                if (isLiked) {
-                  removeReaction();
+    <section className="w-full max-w-[600px] flex flex-col h-full ">
+      <div className="bg-[var(--element-color)] p-4 rounded-2xl mb-8">
+        {item && (
+          <div className="grow">
+            <div className="flex gap-x-4 items-center mb-2">
+              <span className="font-bold text-2xl">{item["name"]}</span>
+              <button
+                onClick={() => {
+                  if (isLiked) {
+                    removeReaction();
+                  } else {
+                    addReaction();
+                  }
+                }}
+                className={`${
+                  isLiked ? "bg-red-600 hover:bg-red-600 active:bg-red-600" : ""
+                } flex items-center gap-x-2`}
+              >
+                {reactionsCount}
+                {isLiked ? <FaHeart /> : <FaRegHeart />}
+              </button>
+            </div>
+            <div className="flex flex-col w-full mb-4 px-2">
+              {Object.keys(item).map((field, index) => {
+                if (
+                  field == "reactions" ||
+                  field == "_id" ||
+                  field == "user" ||
+                  field == "name"
+                ) {
+                  return;
+                } else if (field == "tags") {
+                  return (
+                    <div
+                      key={index}
+                      className="flex gap-x-1 pb-1 border-b border-[var(--border-color)] mb-1"
+                    >
+                      <span className="flex flex-col">
+                        {t("item_tags_label")}:
+                      </span>
+                      <span>{item[field]}</span>
+                    </div>
+                  );
                 } else {
-                  addReaction();
+                  return (
+                    <div
+                      key={index}
+                      className="flex gap-x-1 pb-1 border-b border-[var(--border-color)] mb-1"
+                    >
+                      <span className="flex flex-col">{field}:</span>
+                      <span>{item[field].value}</span>
+                    </div>
+                  );
                 }
-              }}
-              className={`${
-                isLiked ? "bg-red-600 hover:bg-red-600 active:bg-red-600" : ""
-              } flex items-center gap-x-2`}
-            >
-              {reactionsCount}
-              {isLiked ? <FaHeart /> : <FaRegHeart />}
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="bg-[var(--element-color)] p-4 rounded-2xl flex flex-col mb-4">
+        <span className="self-center w-fit font-bold text-xl mb-4 pb-1 border border-[var(--border-color)] rounded-2xl px-2 py-1">
+          {t("comment_title")}
+        </span>
+        <form onSubmit={createComment} className="flex flex-col gap-x-4 md:px-8 w-full">
+          <div className="flex w-full items-center mb-4 gap-x-2 md:gap-x-4">
+            <textarea
+              rows={1}
+              name="content"
+              id="content"
+              placeholder={t("create_placeholder")}
+              className="grow"
+            ></textarea>
+            <button className="disabled:opacity-50 flex justify-center">
+              {loading ? <div className="loader"></div> : t("send_button")}
             </button>
           </div>
-          <div className="flex flex-col w-full mb-4 px-2">
-            {Object.keys(item).map((field, index) => {
-              if (
-                field == "reactions" ||
-                field == "_id" ||
-                field == "user" ||
-                field == "name"
-              ) {
-                return;
-              } else if (field == "tags") {
-                return (
-                  <div
-                    key={index}
-                    className="flex gap-x-1 pb-1 border-b border-[var(--border-color)] mb-1"
-                  >
-                    <span className="flex flex-col">
-                      {t("item_tags_label")}:
-                    </span>
-                    <span>{item[field]}</span>
-                  </div>
-                );
-              } else {
-                return (
-                  <div
-                    key={index}
-                    className="flex gap-x-1 pb-1 border-b border-[var(--border-color)] mb-1"
-                  >
-                    <span className="flex flex-col">{field}:</span>
-                    <span>{item[field].value}</span>
-                  </div>
-                );
-              }
-            })}
+          <span className="mx-10 text-red-500 text-sm text-center">
+            {error}
+          </span>
+        </form>
+        {comments && (
+          <div className="h-full overflow-y-auto flex flex-col gap-y-4">
+            {comments.map((comment, index) => (
+              <div key={index} className="w-full">
+                <Comment
+                  id={comment._id}
+                  userId={comment.user}
+                  onDelete={deleteComment}
+                  comment={comment}
+                  session={data}
+                ></Comment>
+              </div>
+            ))}
           </div>
-        </div>
-      )}
-      <span className="self-center w-fit font-bold text-xl mb-4 pb-1 border border-[var(--border-color)] rounded-2xl px-2 py-1">
-        {t("comment_title")}
-      </span>
-      <form onSubmit={createComment} className="flex flex-col gap-x-4 w-full">
-        <div className="flex w-full items-center mb-4 gap-x-4">
-          <textarea
-            rows={1}
-            name="content"
-            id="content"
-            placeholder={t("create_placeholder")}
-            className="grow"
-          ></textarea>
-          <button className="disabled:opacity-50 flex justify-center">
-            {loading ? <div className="loader"></div> : t("send_button")}
-          </button>
-        </div>
-        <span className="mx-10 text-red-500 text-sm text-center">{error}</span>
-      </form>
-      {comments && (
-        <div className="h-full overflow-y-auto">
-          {comments.map((comment, index) => (
-            <div key={index} className="mb-4 w-full">
-              <Comment
-                id={comment._id}
-                userId={comment.user}
-                onDelete={deleteComment}
-                comment={comment}
-                session={data}
-              ></Comment>
-            </div>
-          ))}
-        </div>
-      )}
+        )}
+      </div>
     </section>
   );
 }
