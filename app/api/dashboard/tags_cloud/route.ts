a59b9350c68d4050c652f7b8e17afa49d06d8c80
SergioD <sergio_david2109@hotmail.com>
2024-08-23 01:26:59 -0600

    refactor: get tags with count

--- app/api/dashboard/tags_cloud/route.ts
+++ app/api/dashboard/tags_cloud/route.ts
@@ -1,12 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from '@/db/db_config'
 import Tag from '@/db/models/tag.models'
+import Item from "@/db/models/item.model";
 
 export async function GET(request: NextRequest) {
     try {
         await connectDB()
-        const tags = await Tag.find().limit(20)
-        return NextResponse.json(tags)
+        const tags = await Tag.find().limit(30)
+        const result = await Item.aggregate([
+            { $unwind: "$tags" },
+            {
+                $group: {
+                    _id: "$tags",
+                    tagCount: {
+                        $sum: 1
+                    }
+                }
+            },
+            {
+                $sort: { itemCount: -1 }
+            },
+            { $limit: 30 },
+        ])
+        return NextResponse.json(result)
     } catch (error) {
         console.error('Error:', error);
         return NextResponse.json({ error: 'Server error' }, { status: 500 });
