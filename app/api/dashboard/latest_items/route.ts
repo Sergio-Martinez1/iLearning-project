c4db71f41f2c741eef0c3e939871fa9ed4eb16c5
SergioD <sergio_david2109@hotmail.com>
2024-08-25 14:00:44 -0600

    fix: add return response

--- app/api/dashboard/latest_items/route.ts
+++ app/api/dashboard/latest_items/route.ts
@@ -9,6 +9,7 @@ export async function GET() {
     if (!items) return NextResponse.json({ error: 'Items not found' }, { status: 404 });
     const response = NextResponse.json(items);
     response.headers.set('Cache-Control', 'no-store'); // Evitar la cache
+    return NextResponse.json(items)
   } catch (error) {
     console.error('Error:', error);
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
