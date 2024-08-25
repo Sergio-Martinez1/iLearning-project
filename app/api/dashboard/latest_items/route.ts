2f87238eeaf36b3427ae70bf4bc59de9d163dc7b
SergioD <sergio_david2109@hotmail.com>
2024-08-25 13:56:31 -0600

    chore: no cache

--- app/api/dashboard/latest_items/route.ts
+++ app/api/dashboard/latest_items/route.ts
@@ -3,13 +3,14 @@ import { connectDB } from '@/db/db_config'
 import Item from '@/db/models/item.model'
 
 export async function GET() {
-    try {
-        await connectDB()
-        const items = await Item.find().populate('user', 'username').populate('group', 'name').sort({ createdAt: -1 }).limit(20)
-        if (!items) return NextResponse.json({ error: 'Items not found' }, { status: 404 });
-        return NextResponse.json(items)
-    } catch (error) {
-        console.error('Error:', error);
-        return NextResponse.json({ error: 'Server error' }, { status: 500 });
-    }
+  try {
+    await connectDB()
+    const items = await Item.find().populate('user', 'username').populate('group', 'name').sort({ createdAt: -1 }).limit(20)
+    if (!items) return NextResponse.json({ error: 'Items not found' }, { status: 404 });
+    const response = NextResponse.json(items);
+    response.headers.set('Cache-Control', 'no-store'); // Evitar la cache
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
 }
