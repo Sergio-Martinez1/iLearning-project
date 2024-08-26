052ae3517141873f0de813e982ba0565ab0a8db4
SergioD <sergio_david2109@hotmail.com>
2024-08-25 22:29:01 -0600

    chore: remove cache

--- app/api/dashboard/latest_items/route.ts
+++ app/api/dashboard/latest_items/route.ts
@@ -1,15 +1,14 @@
 import { NextResponse } from "next/server";
 import { connectDB } from '@/db/db_config'
 import Item from '@/db/models/item.model'
+export const revalidate = 0
 
 export async function GET() {
   try {
     await connectDB()
     const items = await Item.find().populate('user', 'username').populate('group', 'name').sort({ createdAt: -1 }).limit(20)
     if (!items) return NextResponse.json({ error: 'Items not found' }, { status: 404 });
-    const response = NextResponse.json(items);
-    response.headers.set('Cache-Control', 'no-store'); // Evitar la cache
-    return NextResponse.json(items)
+    return NextResponse.json(items);
   } catch (error) {
     console.error('Error:', error);
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
