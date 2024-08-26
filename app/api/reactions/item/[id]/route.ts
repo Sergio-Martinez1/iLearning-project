e0d31b42b0fdf05ab5d2e8414045395633bb70aa
SergioD <sergio_david2109@hotmail.com>
2024-08-25 19:51:47 -0600

    fix: check reactions counter

--- app/api/reactions/item/[id]/route.ts
+++ app/api/reactions/item/[id]/route.ts
@@ -5,57 +5,60 @@ import User from '@/db/models/user.models'
 import Item from '@/db/models/item.model'
 
 export async function POST(req: NextRequest, { params }: { params: { id: Number } }) {
-    try {
-        const session = await getServerSession()
-        if (!session) return NextResponse.json({ error: 'Not authenticated' }, { status: 401 })
+  try {
+    const session = await getServerSession()
+    if (!session) return NextResponse.json({ error: 'Not authenticated' }, { status: 401 })
 
-        await connectDB()
-        const userSession = await User.findOne({ username: session.user?.name })
-        const currentItem = await Item.findOne({ _id: params.id })
-        if (!currentItem) return NextResponse.json({ error: 'Item not found' }, { status: 404 })
-        //if (userSession.role !== 'admin') {
-        //    if (userSession._id.toString() !== currentItem.user.toString()) return NextResponse.json({ error: 'Not allowed' }, { status: 403 })
-        //}
+    await connectDB()
+    const userSession = await User.findOne({ username: session.user?.name })
+    const currentItem = await Item.findOne({ _id: params.id })
+    if (!currentItem) return NextResponse.json({ error: 'Item not found' }, { status: 404 })
+    //if (userSession.role !== 'admin') {
+    //    if (userSession._id.toString() !== currentItem.user.toString()) return NextResponse.json({ error: 'Not allowed' }, { status: 403 })
+    //}
 
-        let reactionExist = userSession.reactions.filter((reactionId: Number) => reactionId === params.id)
-        if (reactionExist.length > 0) return NextResponse.json({ error: 'Reaction already exist' }, { status: 400 })
+    let reactionExist = userSession.reactions.filter((reactionId: Number) => reactionId === params.id)
+    if (reactionExist.length > 0) return NextResponse.json({ error: 'Reaction already exist' }, { status: 400 })
 
-        userSession.reactions.push(params.id)
-        currentItem.reactions += 1
-        await currentItem.save()
-        await userSession.save()
+    userSession.reactions.push(params.id)
+    currentItem.reactions += 1
+    await currentItem.save()
+    await userSession.save()
 
-        return NextResponse.json(currentItem)
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: 'Server error' }, { status: 500 })
-    }
+    return NextResponse.json(currentItem)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: 'Server error' }, { status: 500 })
+  }
 }
 
 export async function DELETE(req: NextRequest, { params }: { params: { id: Number } }) {
-    try {
-        const session = await getServerSession()
-        if (!session) return NextResponse.json({ error: 'Not authenticated' }, { status: 401 })
+  try {
+    const session = await getServerSession()
+    if (!session) return NextResponse.json({ error: 'Not authenticated' }, { status: 401 })
 
-        await connectDB()
-        const userSession = await User.findOne({ username: session.user?.name })
-        const currentItem = await Item.findOne({ _id: params.id })
-        if (!currentItem) return NextResponse.json({ error: 'Item not found' }, { status: 404 })
-        //if (userSession.role !== 'admin') {
-        //    if (userSession._id.toString() !== currentItem.user.toString()) return NextResponse.json({ error: 'Not allowed' }, { status: 403 })
-        //}
+    await connectDB()
+    const userSession = await User.findOne({ username: session.user?.name })
+    const currentItem = await Item.findOne({ _id: params.id })
+    if (!currentItem) return NextResponse.json({ error: 'Item not found' }, { status: 404 })
+    //if (userSession.role !== 'admin') {
+    //    if (userSession._id.toString() !== currentItem.user.toString()) return NextResponse.json({ error: 'Not allowed' }, { status: 403 })
+    //}
 
-        let reactionExist = userSession.reactions.filter((reactionId: Number) => reactionId === params.id)
-        if (reactionExist.length == 0) return NextResponse.json({ error: 'Reaction does not exist' }, { status: 400 })
+    let reactionExist = userSession.reactions.filter((reactionId: Number) => reactionId === params.id)
+    if (reactionExist.length == 0) return NextResponse.json({ error: 'Reaction does not exist' }, { status: 400 })
 
-        userSession.reactions = userSession.reactions.filter((reactionId: Number) => reactionId !== params.id)
-        currentItem.reactions -= 1
-        await currentItem.save()
-        await userSession.save()
+    userSession.reactions = userSession.reactions.filter((reactionId: Number) => reactionId !== params.id)
+    if (currentItem.reactions > 0) {
 
-        return NextResponse.json(currentItem)
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: 'Server error' }, { status: 500 })
+      currentItem.reactions -= 1
     }
+    await currentItem.save()
+    await userSession.save()
+
+    return NextResponse.json(currentItem)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: 'Server error' }, { status: 500 })
+  }
 }
